797a8d72fb0568283972094b76257175606b3157
343-Guilty4Spark <edward.manca@studenti.polito.it>
2022-08-04 09:45:17 +0200

    quantization of other layers

--- pytorch_ex/quantized_layer_templates_pytorch.py
+++ pytorch_ex/quantized_layer_templates_pytorch.py
@@ -44,7 +44,6 @@ class Quant_ResidualBlock_custom(nn.Module):
         out = self.act(out + res)
         return out
 
-
 class Quant_Residual131_custom(nn.Module):
     def __init__(self, in_channels, int_channels, out_channels, stride=(1, 1), bias=True):
         super(Quant_Residual131_custom, self).__init__()
@@ -182,3 +181,96 @@ class Quant_ResidualBlock_brevitas(nn.Module):
         res = self.se(res) if self.squeeze_and_excite else res
         out = self.act(out + res)
         return out
+
+class Quant_Residual131_brevitas(nn.Module):
+    def __init__(self, in_channels, int_channels, out_channels, stride=(1, 1), bias=True):
+        super(Quant_Residual131_brevitas, self).__init__()
+        self.l1 = nn.Conv2d(in_channels, int_channels, kernel_size=(1, 1), padding=(0, 0), stride=(1, 1), bias=bias)
+        self.bn1 = nn.BatchNorm2d(num_features=int_channels, momentum=0.01, eps=1e-3)
+        self.act1 = nn.ReLU()
+        self.l2 = nn.Conv2d(int_channels, int_channels, kernel_size=(3, 3), padding=(1, 1), stride=stride, bias=bias)
+        self.bn2 = nn.BatchNorm2d(num_features=int_channels, momentum=0.01, eps=1e-3)
+        self.act2 = nn.ReLU()
+        self.l3 = nn.Conv2d(int_channels, out_channels, kernel_size=(1, 1), padding=(0, 0), stride=(1, 1), bias=bias)
+        self.bn3 = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.bn1(out)
+        out = self.act1(out)
+        out = self.l2(out)
+        out = self.bn2(out)
+        out = self.act2(out)
+        out = self.l3(out)
+        out = self.bn3(out)
+        return out
+
+
+class Quant_Residual33_brevitas(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, bias=True):
+        super(Quant_Residual33_brevitas, self).__init__()
+        self.l1 = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding=(1, 1), stride=stride, bias=bias)
+        self.bn1 = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+        self.act1 = nn.ReLU()
+        self.l2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1), bias=bias)
+        self.bn2 = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+
+    def forward(self, x):
+        out = self.l1(x)
+        out = self.bn1(out)
+        out = self.act1(out)
+        out = self.l2(out)
+        out = self.bn2(out)
+        return out
+
+class Quant_SeparableConv2d_brevitas(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding=(1, 1), halve_resolution=False, bias=True, replicas=1):
+        super(Quant_SeparableConv2d_brevitas, self).__init__()
+        self.separable_conv = nn.Sequential()
+        # first replica is always present
+        self.separable_conv.append(nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=(1, 1),
+                                             groups=in_channels, bias=bias, padding=padding))
+        # self.separable_conv.append(nn.BatchNorm2d(num_features=in_channels, momentum=0.01, eps=1e-3))
+        # self.separable_conv.append(nn.ReLU())
+        self.separable_conv.append(nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), bias=bias))
+        self.separable_conv.append(nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3))
+        if replicas == 1 and halve_resolution:
+            self.separable_conv.append(nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)))
+        if replicas > 1:
+            self.separable_conv.append(nn.ReLU())
+        for i in range(1, replicas):
+            self.separable_conv.append(nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, stride=(1,1),
+                                                 groups=out_channels, bias=bias, padding=padding))
+            # self.separable_conv.append(nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3))
+            # self.separable_conv.append(nn.ReLU())
+            self.separable_conv.append(nn.Conv2d(out_channels, out_channels, kernel_size=(1, 1), bias=bias))
+            self.separable_conv.append(nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3))
+            if i < (replicas - 1):
+                self.separable_conv.append(nn.ReLU())
+            if i == (replicas - 1) and halve_resolution:
+                self.separable_conv.append(nn.MaxPool2d(kernel_size=(2,2), stride=(2,2)))
+
+    def forward(self, x):
+        out = self.separable_conv(x)
+        return out
+
+class Quant_SqueezeAndExcite_brevitas(nn.Module): # TODO: complete SqueezeAndExcite
+    def __init__(self, channels, ratio, bias=True):
+        super(Quant_SqueezeAndExcite_brevitas, self).__init__()
+        # self.glob_pool = nn.AvgPool2d(kernel_size=(glob_avg_window, glob_avg_window))
+        self.fc1 = nn.Linear(channels, int(float(channels)/float(ratio)), bias=bias)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(int(float(channels)/float(ratio)), channels, bias=bias)
+        self.sigmoid = nn.Sigmoid()
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        out = F.adaptive_avg_pool2d(x, 1)
+        out = self.flatten(out)
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.sigmoid(out)
+        out = out.reshape((out.shape[0], out.shape[1], 1, 1))
+
+        return out * x
