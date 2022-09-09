23e82e0d77d052394d9b0f47e956d095cb28614e
NicDox <nicola.domini@live.it>
2022-09-09 12:23:43 +0200

    Upload quantized versions of SqueezeAndExcite

--- pytorch_ex/quantized_layer_templates_pytorch.py
+++ pytorch_ex/quantized_layer_templates_pytorch.py
@@ -1,5 +1,9 @@
 from torch import nn
 from torch.nn import functional as F
+import quantization as cs
+import brevitas.nn as qnn
+from brevitas.quant.scaled_int import Int8Bias, Int16Bias, Int32Bias, Int8WeightPerTensorFloat, Uint8ActPerTensorFloat, Int8ActPerTensorFloat
+
 
 class Quant_ResidualBlock_custom(nn.Module):
     def __init__(self, in_channels, out_channels, int_channels=None, SP_kernel_size=(3, 3), padding=(1, 1),
@@ -27,7 +31,7 @@ class Quant_ResidualBlock_custom(nn.Module):
             else:
                 self.up = cs.Conv2d(in_channels, out_channels, stride=(1, 1), kernel_size=(1, 1), bias=bias, act_bit=self.act_bit, weight_bit=self.weight_bit,
                                     bias_bit=self.bias_bit, quantization=self.quantization, quant_method=self.quant_method)
-            self.bn_up = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+            #self.bn_up = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
         if self.halve_resolution and not self.upscale:
             self.pool = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
 
@@ -37,7 +41,7 @@ class Quant_ResidualBlock_custom(nn.Module):
 
     def forward(self, x):
         out = self.res_block(x)
-        res = self.bn_up(self.up(x)) if self.upscale else x
+        #res = self.bn_up(self.up(x)) if self.upscale else x
         res = self.pool(res) if (self.halve_resolution and not self.upscale) else res
         ###collegamento epr squeeze and excite..
         res = self.se(res) if self.squeeze_and_excite else res
@@ -56,30 +60,28 @@ class Quant_Residual131_custom(nn.Module):
         self.alpha_coeff = 10.0
 
         self.l1 = cs.Conv2d(in_channels, int_channels, kernel_size=(1, 1), padding=(0, 0), stride=(1, 1), bias=bias, act_bit=self.act_bit, weight_bit=self.weight_bit, bias_bit=self.bias_bit, quantization=self.quantization, quant_method=self.quant_method)
-        #NICOLA : folding batchnorm
-        self.bn1 = nn.BatchNorm2d(num_features=int_channels, momentum=0.01, eps=1e-3)
+        #self.bn1 = nn.BatchNorm2d(num_features=int_channels, momentum=0.01, eps=1e-3)
         self.act1 = cs.ReLu(alpha=self.alpha_coeff, act_bit=self.act_bit, quantization=self.quantization)
         self.l2 = cs.Conv2d(int_channels, int_channels, kernel_size=(3, 3), padding=(1, 1), stride=stride, bias=bias, act_bit=self.act_bit, weight_bit=self.weight_bit, bias_bit=self.bias_bit, quantization=self.quantization, quant_method=self.quant_method)
-        self.bn2 = nn.BatchNorm2d(num_features=int_channels, momentum=0.01, eps=1e-3)
+        #self.bn2 = nn.BatchNorm2d(num_features=int_channels, momentum=0.01, eps=1e-3)
         self.act2 = cs.ReLU(alpha=self.alpha_coeff, act_bit=self.act_bit, quantization=self.quantization)
         self.l3 = cs.Conv2d(int_channels, out_channels, kernel_size=(1, 1), padding=(0, 0), stride=(1, 1), bias=bias, act_bit=self.act_bit, weight_bit=self.weight_bit, bias_bit=self.bias_bit, quantization=self.quantization, quant_method=self.quant_method)
-        #NICOLA : folding batchnorm
-        self.bn3 = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+        #self.bn3 = nn.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
 
     def forward(self, x):
         #NICOLA : vedi se devi quantizzare x
         out = self.l1(x)
-        out = self.bn1(out)
+        #out = self.bn1(out)
         out = self.act1(out)
         out = self.l2(out)
-        out = self.bn2(out)
+        #out = self.bn2(out)
         out = self.act2(out)
         out = self.l3(out)
-        out = self.bn3(out)
+        #out = self.bn3(out)
         return out
 
 class Quant_Residual33_custom(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, bias=True):#NICOLA : aggiungere parametro quantization
+    def __init__(self, in_channels, out_channels, stride, bias=True):
         super(Quant_Residual33_custom, self).__init__()
 
         #NICOLA : questa roba metila sul residual block, che la passa poi ai sottoblocks
@@ -91,19 +93,18 @@ class Quant_Residual33_custom(nn.Module):
         self.alpha_coeff = 10.0
 
         self.l1 = cs.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding=(1, 1), stride=stride, bias=bias, act_bit=self.act_bit, weight_bit=self.weight_bit, bias_bit=self.bias_bit, quantization=self.quantization, quant_method=self.quant_method)
-        #NICOLA : FOLDARE BATCH NORM
-        self.bn1 = cs.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+        #self.bn1 = cs.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
         self.act1 = cs.ReLu(alpha=self.alpha_coeff, act_bit=self.act_bit, quantization=self.quantization)
         self.l2 = cs.Conv2d(out_channels, out_channels, kernel_size=(3, 3), padding=(1, 1), stride=(1, 1), bias=bias, act_bit=self.act_bit, weight_bit=self.weight_bit, bias_bit=self.bias_bit, quantization=self.quantization, quant_method=self.quant_method)
-        self.bn2 = cs.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
+        #self.bn2 = cs.BatchNorm2d(num_features=out_channels, momentum=0.01, eps=1e-3)
 
     def forward(self, x):
         #NICOLA : vedi se devi quantizzare x o no
         out = self.l1(x)
-        out = self.bn1(out)
+        #out = self.bn1(out)
         out = self.act1(out)
         out = self.l2(out)
-        out = self.bn2(out)
+        #out = self.bn2(out)
         return out
 
 class Quant_SeparableConv2d_custom(nn.Module):
@@ -140,10 +141,18 @@ class Quant_SeparableConv2d_custom(nn.Module):
 class Quant_SqueezeAndExcite_custom(nn.Module): # TODO: complete SqueezeAndExcite
     def __init__(self, channels, ratio, bias=True):
         super(Quant_SqueezeAndExcite_custom, self).__init__()
+
+        self.weight_bit = 8
+        self.act_bit = 8
+        self.bias_bit = 16
+        self.quant_method = 'scale'
+        self.quantization = True
+        self.alpha_coeff = 10.0
+
         # self.glob_pool = nn.AvgPool2d(kernel_size=(glob_avg_window, glob_avg_window))
-        self.fc1 = nn.Linear(channels, int(float(channels)/float(ratio)), bias=bias)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(int(float(channels)/float(ratio)), channels, bias=bias)
+        self.fc1 = cs.Linear(channels, int(float(channels)/float(ratio)), bias=bias)
+        self.relu = cs.ReLU(alpha=self.alpha_coeff, act_bit=self.act_bit, quantization=self.quantization)
+        self.fc2 = cs.Linear(int(float(channels)/float(ratio)), channels, bias=bias)
         self.sigmoid = nn.Sigmoid()
         self.flatten = nn.Flatten()
 
@@ -170,11 +179,11 @@ class Quant_ResidualBlock_brevitas(nn.Module):
         stride = (2, 2) if self.halve_resolution else (1, 1)
         # Set up the main sequence of layers
         if block_type == "Residual33":
-            self.res_block = Quant_Residual33(in_channels, out_channels, stride, bias)
+            self.res_block = Quant_Residual33_brevitas(in_channels, out_channels, stride, bias)
         elif block_type == "Residual131":
-            self.res_block = Quant_Residual131(in_channels, int_channels, out_channels, stride, bias)
+            self.res_block = Quant_Residual131_brevitas(in_channels, int_channels, out_channels, stride, bias)
         elif block_type == "SeparableConv2d":
-            self.res_block = Quant_SeparableConv2d(in_channels, out_channels, SP_kernel_size, padding, halve_resolution, bias, SP_replicas)
+            self.res_block = Quant_SeparableConv2d_brevitas(in_channels, out_channels, SP_kernel_size, padding, halve_resolution, bias, SP_replicas)
         else:
             exit("invalid block")
         # Configure the residual path
@@ -191,7 +200,7 @@ class Quant_ResidualBlock_brevitas(nn.Module):
             self.pool = qnn.QuantMaxPool2d(kernel_size=(2, 2), stride=2)
 
         if self.squeeze_and_excite:
-            self.se = Quant_SqueezeAndExcite(out_channels, SE_ratio, bias)
+            self.se = Quant_SqueezeAndExcite_brevitas(out_channels, SE_ratio, bias)
         self.act = nn.ReLU()
 
     def forward(self, x):
@@ -316,11 +325,21 @@ class Quant_SeparableConv2d_brevitas(nn.Module):
 class Quant_SqueezeAndExcite_brevitas(nn.Module): # TODO: complete SqueezeAndExcite
     def __init__(self, channels, ratio, bias=True):
         super(Quant_SqueezeAndExcite_brevitas, self).__init__()
+
+        #NICOLA : spostare i seguenti parametri su classi superiori
+        self.weigh_bit = Int8WeightPerTensorFloat 
+        self.act_bit = Uint8ActPerTensorFloat
+        self.bias_bit = Int16Bias
+        self.quant_method = 'scale'
+        self.alpha_coeff = 123.0 # controlla alpha value   
+
         # self.glob_pool = nn.AvgPool2d(kernel_size=(glob_avg_window, glob_avg_window))
-        self.fc1 = nn.Linear(channels, int(float(channels)/float(ratio)), bias=bias)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(int(float(channels)/float(ratio)), channels, bias=bias)
-        self.sigmoid = nn.Sigmoid()
+        self.fc1 = qnn.QuantLinear(channels, int(float(channels)/float(ratio)), bias=bias, weight_quant=self.weigh_bit, bias_quant=self.bias_bit, 
+                                    output_quant=self.act_bit, return_quant_tensor=True)
+        self.relu = qnn.QuantReLU(alpha=self.alpha_coeff)
+        self.fc2 = qnn.QuantLinear(int(float(channels)/float(ratio)), channels, bias=bias, weight_quant=self.weigh_bit, bias_quant=self.bias_bit, 
+                                    output_quant=self.act_bit, return_quant_tensor=True)
+        self.sigmoid = qnn.QuantSigmoid(act_quant=self.act_bit, input_quant=self.act_bit, return_quant_tensor=True)
         self.flatten = nn.Flatten()
 
     def forward(self, x):
